fba97d4a61f76f576e93d0a09055b593c4f02f01
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-05 10:22:20 +0200

    Revert "shipping status checker should also find about 'delivered' orders"
    This reverts commit e21e2b3d913b087f164df4989a5b74ff3196b5d7.

--- server/runners/shipping.runner.js
+++ server/runners/shipping.runner.js
@@ -96,7 +96,7 @@ export default class ShippingRunner {
       );
 
       const orders: Array<OrderDoc> = await Order.find({
-        status: { $in: ['confirmed', 'shipped', 'delivered'] },
+        status: { $in: ['confirmed', 'shipped'] },
         // shippingStatus: { $in: [NP.generated, NP.shipped] },
         shippingUpdatedAt: { $lte: previousDate },
       });
