8ba3c07a3b4da4366a0c7df07b7107be0ae795a2
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-27 15:57:23 +0200

    fix

--- server/helpers/shipping.js
+++ server/helpers/shipping.js
@@ -64,10 +64,10 @@ export default class Shipping {
       case '1':
         return NP.generated;
       case '4':
+      case '5':
       case '6':
       case '101':
         return NP.shipped;
-      case '5':
       case '7':
       case '8':
         return NP.delivered;
